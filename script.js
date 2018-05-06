7ac276537a9c06ba749f52422ab720851725a34b
Jocelyn Tan <jocelyntan89@hotmail.com>
2018-05-06 16:02:02 +0800

    able to toggle lines on chart

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ var thaiOrder = 2;
 var vietOrder = 3;
 var min = Math.min(indiaOrder, thaiOrder, vietOrder) - 1;
 var max = Math.max(indiaOrder, thaiOrder, vietOrder) + 1;
+var countries = ['India', 'Thailand', 'Vietnam'];
+var data;
 
+// interaction from clicking the legend
 india.on('click', (e) => {
   if (india.css('opacity') == 0.2) {
     indiaOrder = min;
     india.css('order', indiaOrder);
     india.css('opacity', 1);
+    countries.push('India');
+    update();
   } else {
     indiaOrder = max;
     india.css('order', indiaOrder);
     india.css('opacity', 0.2);
+    var index = countries.indexOf('India');
+    countries.splice(index, 1);
+    update();
   }
 })
 thailand.on('click', (e) => {
@@ -23,10 +31,15 @@ thailand.on('click', (e) => {
     thaiOrder = min;
     thailand.css('order', thaiOrder);
     thailand.css('opacity', 1);
+    countries.push('Thailand');
+    update();
   } else {
     thaiOrder = max;
     thailand.css('order', thaiOrder);
     thailand.css('opacity', 0.2);
+    var index = countries.indexOf('Thailand');
+    countries.splice(index, 1);
+    update();
   }
 })
 vietnam.on('click', (e) => {
@@ -34,21 +47,108 @@ vietnam.on('click', (e) => {
     vietOrder = min;
     vietnam.css('order', vietOrder);
     vietnam.css('opacity', 1);
+    countries.push('Vietnam');
+    update();
   } else {
     vietOrder = max;
     vietnam.css('order', vietOrder);
     vietnam.css('opacity', 0.2);
+    var index = countries.indexOf('Vietnam');
+    countries.splice(index, 1);
+    update();
   }
 })
 
+// create responsive chart area
 var height = $('#chart-area').height();
 var width = $('#chart-area').width();
 var svg = d3.select('#chart-area').append("svg")
     .attr("width", '100%')
     .attr("height", '100%')
-    .attr('viewBox','0 0 '+Math.min(width,height)+' '+Math.min(width,height))
+    .attr('viewBox','0 0 '+ Math.min(width,height)+ ' '+ Math.min(width,height))
     .attr('preserveAspectRatio','xMinYMin');
 
-d3.csv('milledRiceEndingStocks.csv').then(data => {
-  console.log(data)
+// Set scales of X and Y axis
+var x = d3.scaleLinear().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// Create axis
+var xAxisCall = d3.axisBottom(x);
+var yAxisCall = d3.axisLeft(y);
+
+var xAxis = svg.append('g')
+  .attr('class', 'x-axis')
+  .attr('transform', 'translate(0,' + (height - 50) + ')')
+var yAxis = svg.append('g')
+  .attr('class', 'y-axis')
+  .attr('transform', 'translate(50,' + 0 + ')')
+
+// Line path generator
+var indiaLine = d3.line()
+  .x(d => { return x(d.Year) })
+  .y(d => { return y(d.India) });
+var thaiLine = d3.line()
+  .x(d => { return x(d.Year) })
+  .y(d => { return y(d.Thailand) });
+var vietLine = d3.line()
+  .x(d => { return x(d.Year) })
+  .y(d => { return y(d.Vietnam) });
+
+d3.csv('milledRiceEndingStocks.csv').then(dataz => {
+  // data cleaning to change values to integer
+  dataz.forEach(row => {
+    row.Year = +row.Year;
+    row.India = +row.India;
+    row.Thailand = +row.Thailand;
+    row.Vietnam = +row.Vietnam;
+  });
+  data = dataz;
+  update();
 })
+
+function update(){
+  // find the max of the existing lines for the domain
+  var max = [];
+  countries.forEach(country => {
+    max.push(d3.max(data, d => { return (d[country]) }))
+  })
+
+  // Set the domains
+  x.domain(d3.extent(data, d => { return d.Year }));
+  y.domain([0, Math.max.apply(null, max)])
+
+  // Call the axis
+  xAxis.call(xAxisCall);
+  yAxis.call(yAxisCall);
+
+  // Clear all previous lines
+  d3.selectAll("path.line").remove();
+
+  // Add line to chart
+  if(countries.includes('India')){
+    svg.append("path")
+      .attr("class", "line")
+      .attr("fill", "none")
+      .attr("stroke", "#fabe9c")
+      .attr("stroke-width", "2px")
+      .attr("d", indiaLine(data));
+  }
+
+  if(countries.includes('Thailand')){
+    svg.append("path")
+      .attr("class", "line")
+      .attr("fill", "none")
+      .attr("stroke", "#f6ab9a")
+      .attr("stroke-width", "2px")
+      .attr("d", thaiLine(data));
+  }
+
+  if (countries.includes('Vietnam')){
+    svg.append("path")
+      .attr("class", "line")
+      .attr("fill", "none")
+      .attr("stroke", "#fed47d")
+      .attr("stroke-width", "2px")
+      .attr("d", vietLine(data));
+  }
+}
